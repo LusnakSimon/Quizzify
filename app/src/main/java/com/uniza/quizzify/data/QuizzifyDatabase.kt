26e3341434c41c26ca574f527668f390a9849f17
Šimon Lušňák <lusnak@stud.uniza.sk>
2024-06-03 11:46:09 +0200

    vytvorenie Databazy

---
+++ app/src/main/java/com/uniza/quizzify/data/QuizzifyDatabase.kt
@@ -0,0 +1,36 @@
+package com.uniza.quizzify.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class, UserProgress::class, Category::class, Question::class, Answer::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun userProgressDao(): UserProgressDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun questionDao(): QuestionDao
+    abstract fun answerDao(): AnswerDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "quizzify_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
