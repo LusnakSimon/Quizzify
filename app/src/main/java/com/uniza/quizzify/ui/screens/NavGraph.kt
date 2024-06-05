47b423a582e7d1b0d64a40eabd0a756e9316cf32
Šimon Lušňák <lusnak@stud.uniza.sk>
2024-06-05 23:11:10 +0200

    navigacia

---
+++ app/src/main/java/com/uniza/quizzify/ui/screens/NavGraph.kt
@@ -0,0 +1,26 @@
+package com.uniza.quizzify.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph(startDestination: String = "initial") {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable("initial") { InitialScreen(navController) }
+        composable("signIn") { SignInScreen(navController) }
+        composable("register") { RegisterScreen(navController) }
+        composable("mainMenu") { MainMenuScreen(navController) }
+        composable("settings") { SettingsScreen(navController) }
+        composable("leaderboards") { LeaderboardScreen(navController) }
+        composable("profile") { ProfileScreen(navController) }
+        composable("changeUsername") { ChangeUsernameScreen(navController) }
+        composable("changePassword") { ChangePasswordScreen(navController) }
+        composable("categories") { CategoryScreen(navController) }
+        composable("question") { QuestionScreen(navController)}
+
+    }
+}
